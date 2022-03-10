5fb24e5b323605625101261e67faf73a15fdef9e
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-10 13:04:48 +0300

    Updated 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/*
+ * main - main function
+ *
+ * the main function
+ * returns 0
+ */
+
 int main() {
   puts("\"Programming is like building a multilingual puzzle");
   return (0);
