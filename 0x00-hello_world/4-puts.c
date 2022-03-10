65291bfb0b429f3aea025f54ff087517efcab573
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-10 13:36:07 +0300

    Updated 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,6 @@
  * Description: the main function
  * puts text to stout. Don't forget that a line should not exceed 80 characters.
  * But you're totally free to use several lines to properly
- * describe your function
  * Return: 0
  */
 
