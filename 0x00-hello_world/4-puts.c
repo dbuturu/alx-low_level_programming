770bd8a2bc7e66a3ff9f5254d558b3c3ad58826d
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-10 13:34:49 +0300

    Updated 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-/*
+/**
  * main - main function
  *
  * Description: the main function
- * puts text to stout
- * returns 0
+ * puts text to stout. Don't forget that a line should not exceed 80 characters.
+ * But you're totally free to use several lines to properly
+ * describe your function
+ * Return: 0
  */
 
 int main(void)
