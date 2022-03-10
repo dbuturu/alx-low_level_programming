fdc75163f94bac0a5b12e0794d8a3b5a21eb4909
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-10 13:43:45 +0300

    Added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Description: the main function
+ * puts text to stout.
+ * But you're totally free to use several lines to properly
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
