4caeeb3a5439e911b66a6390cd649d96b8f23637
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-12 01:33:40 +0300

    Added 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Description: the main function
+ * puts text to stout.
+ * But you're totally free to use several lines to properly
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 9; ++x)
+		for (y = 0; y <= 9; ++y)
+		{
+			putchar(x + '0');
+			putchar(y + '0');
+			if (!((x == 9) & (y == 9)))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	putchar('\n');
+	return (0);
+}
+
