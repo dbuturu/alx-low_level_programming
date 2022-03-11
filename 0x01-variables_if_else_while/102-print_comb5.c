fa661d376266419af73e6fb1a6f681fc823324b6
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-12 02:21:15 +0300

    Updated 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,20 +11,24 @@
 
 int main(void)
 {
-	int x;
-	int y;
+	int x, y, i, j;
 
 	for (x = 0; x <= 9; ++x)
 		for (y = 0; y <= 9; ++y)
-		{
-			putchar(x + '0');
-			putchar(y + '0');
-			if (!((x == 9) & (y == 9)))
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
+			for (i = 0; i <= 9; ++i)
+				for (j = 0; j <= 9; ++j)
+				{
+					putchar(x + '0');
+					putchar(y + '0');
+					putchar(' ');
+					putchar(i + '0');
+					putchar(j + '0');
+					if (!((x == 9) && (y == 9) && (i == 8) && (j == 9)))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
 	putchar('\n');
 	return (0);
 }
