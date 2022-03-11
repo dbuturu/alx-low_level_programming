d5d1cd2a72ecfb505df3894ab5d6abb89255cf79
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-12 00:40:17 +0300

    Added 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Description: the main function
+ * puts text to stout.
+ * But you're totally free to use several lines to properly
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; ++n)
+	{
+		putchar(n + '0');
+		if (n < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
