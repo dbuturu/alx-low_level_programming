b383a6683e2b2c97cc6beb13cbe35a4b3d900d6d
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-11 22:59:08 +0300

    Added 	1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Description: the main function
+ * puts text to stout.
+ * But you're totally free to use several lines to properly
+ * Return: 0
+ */int main(void)
+{
+	int n;
+	int last_digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_digit = n % 10;
+	if (last_digit>5)
+	{
+		printf("%d and is greater than 5\n",last_digit);
+	}
+	else if (last_digit==0)
+	{
+		printf("%d and is 0\n",last_digit);
+	}
+	else
+	{
+		printf("%d and is less than 6 and not 0\n",last_digit);
+	}
+	return (0);
+}
+
