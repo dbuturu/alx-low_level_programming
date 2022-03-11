501cd333f0aa502c0d25643fc3e18836f5efe2ff
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-11 23:51:09 +0300

    Added 	4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Description: the main function
+ * puts text to stout.
+ * But you're totally free to use several lines to properly
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		if ((c == 'e') | (c == 'q'))
+			continue;
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
+
