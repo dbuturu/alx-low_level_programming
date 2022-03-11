355482e3d7a9fbed792af5172cf5e6dc5ef67907
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-11 23:59:09 +0300

    Added 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Description: the main function
+ * puts text to stout.
+ * But you're totally free to use several lines to properly
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; ++c)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
+
