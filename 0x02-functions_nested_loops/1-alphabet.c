77ee051cf5e60a19ea281fb09dc1a55e37655fb4
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-15 23:45:44 +0300

    Added  1-alphabet.c 1-main.c Updated main.h

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - print_alphabet function
+ *
+ * Description: the print_alphabet function
+ * puts text to stout.
+ * But you're totally free to use several lines to properly
+ * Return: 0
+ */
+
+void print_alphabet()
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+		putchar(c);
+	putchar('\n');
+}
+
