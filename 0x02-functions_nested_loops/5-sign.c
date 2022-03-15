d28b4d87db0ef470b228182d34f593f560190789
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-16 02:12:13 +0300

    Added 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *  print_sign-  print_signfunction
+ *
+ * Description: the  print_signfunction
+ * @i: int i
+ * Return: 0
+ */
+int print_sign(int i)
+{
+	if (i < 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (i == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else
+	{
+		putchar('-');
+		return (-1);
+	}
+}
+
