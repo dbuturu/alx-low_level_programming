6d7b169ea1e6f3b85f65eabc70a1f56941fc7444
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-16 02:50:54 +0300

    Added 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - main function
+ *
+ * Description: the main function
+ * puts text to stout.
+ * @i: int i
+ * Return: 0
+ */
+
+int _abs(int i)
+{
+	return (abs(i));
+}
+
