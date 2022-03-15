a6474c21729dad174589f2361b7af3e21ecd3877
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-15 18:36:26 +0300

    Added 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Description: the main function
+ * puts text to stout.
+ * But you're totally free to use several lines to properly
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("_putchar\n");
+	return (0);
+}
+
