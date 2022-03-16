42a632e6b3249487d5c9b38db2107f9194ee74eb
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-16 12:31:41 +0300

    Added 11-main.c 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print_to_98 function
+ *
+ * Description: the print_to_98 function
+ * puts text to stout.
+ * @n: int n
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		_putchar('0' + (i / 10));
+		_putchar('0' + (i % 10));
+		if (i != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
+
