8c89b64e9578f4bef54f31acc000c7a19b041d69
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-16 09:48:28 +0300

    Updated 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,17 +10,17 @@
 void jack_bauer(void)
 {
 	int hours = 0;
-	int minutes= 0;
+	int minutes = 0;
 
-	for (hours=0;hours<=24; ++hours)
+	for (hours = 0; hours < 24; ++hours)
 	{
-		for (minutes=0;minutes<=60; ++minutes)
+		for (minutes = 0; minutes < 60; ++minutes)
 		{
 			_putchar('0' + (hours / 10));
 			_putchar('0' + (hours % 10));
 			_putchar(':');
-			_putchar('0' + (minutes/ 10));
-			_putchar('0' + (minutes% 10));
+			_putchar('0' + (minutes / 10));
+			_putchar('0' + (minutes % 10));
 			_putchar('\n');
 		}
 	}
