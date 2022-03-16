ef922c8a190329fe37edac141afea94f700d742d
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-16 22:59:14 +0300

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,22 +8,24 @@
 int main(void)
 {
 	int i;
-	long fibonacci_0, fibonacci_1, fibonacci_2;
+	long fibonacci_0, fibonacci_1, fibonacci_2, sum;
 
 	i = 0;
 	fibonacci_0 = 1;
 	fibonacci_1 = 2;
 	fibonacci_2 = 0;
+	sum = 0;
 
 	while (fibonacci_1 < 4000000)
 	{
 		fibonacci_2 = fibonacci_0 + fibonacci_1;
-		printf("%li, ", fibonacci_0);
+		if (fibonacci_1 % 2 == 0)
+			sum += fibonacci_1;
 		fibonacci_0 = fibonacci_1;
 		fibonacci_1 = fibonacci_2;
 		++i;
 	}
-	printf("%li", fibonacci_0);
+	printf("%li", sum);
 	putchar('\n');
 	return (0);
 }
