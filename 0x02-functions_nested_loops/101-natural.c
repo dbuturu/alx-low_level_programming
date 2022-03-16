b99e9449feee4ab3afbfbff48c29ee5dccc6c2ea
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-16 17:11:09 +0300

    Added 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void) {
+  int i, sum;
+
+  for (i = 0; i < 1024; ++i) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum += i;
+    }
+  }
+  printf("%d", sum);
+  return (0);
+}
