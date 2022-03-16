e6b8f32cda0169ec47fbeb7158f23e02352afa7b
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-16 11:33:41 +0300

    Added 10-add.c 10-main.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * add - add function
+ *
+ * Description: the add function
+ * puts text to stout.
+ * Return: void
+ */
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
