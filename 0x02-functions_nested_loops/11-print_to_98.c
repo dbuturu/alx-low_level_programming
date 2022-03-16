32407271c25216313f57bfdc572aef3eec17bff8
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-16 15:26:40 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,19 +10,26 @@
  * Return: void
  */
 
-void print_to_98(int n)
-{
+void print_to_98(int n) {
 	int i;
 
-	for (i = n; i <= 98; i++)
-	{
-		_putchar('0' + (i / 10));
-		_putchar('0' + (i % 10));
-		if (i != 98)
-		{
-			_putchar(',');
-			_putchar(' ');
+	if (n < 98) {
+		for (i = n; i <= 98; i++) {
+			printf("%d", i);
+			if (i != 98) {
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
+	} else {
+		for (i = n; i >= 98; i--) {
+			printf("%d", i);
+			if (i != 98) {
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+
 	}
 	_putchar('\n');
 	fprintf(stderr, "\n");
