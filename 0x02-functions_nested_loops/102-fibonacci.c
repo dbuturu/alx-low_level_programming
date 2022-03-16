47c146b1a07b72a6d3e8f8fde37b9bfa0aad6172
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-16 17:55:55 +0300

    Updated 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,20 +5,22 @@
  *
  * Return: Always 0.
  */
-int main(void)
-{
-	int i, fibonacci_0, fibonacci_1, fibonacci_2;
+int main(void) {
+  int i, fibonacci_0, fibonacci_1, fibonacci_2;
 
-	fibonacci_0 = 1;
-	fibonacci_1 = 2;
-	fibonacci_2 = 0;
-	for (i = 0; i <= 50; ++i)
-	{
-		fibonacci_2 = fibonacci_0 + fibonacci_1;
-		printf("%d ", fibonacci_0);
-		fibonacci_0 = fibonacci_1;
-		fibonacci_1 = fibonacci_2;
-	}
-	return (0);
+  fibonacci_0 = 1;
+  fibonacci_1 = 2;
+  fibonacci_2 = 0;
+  for (i = 0; i <= 50; ++i)
+  {
+    fibonacci_2 = fibonacci_0 + fibonacci_1;
+    if (fibonacci_0 == 1)
+      printf("%d", fibonacci_0);
+    printf(", %d", fibonacci_0);
+    fibonacci_0 = fibonacci_1;
+    fibonacci_1 = fibonacci_2;
+  }
+  putchar('\n');
+  return (0);
 }
 
