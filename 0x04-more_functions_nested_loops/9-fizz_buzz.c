84e8700e1c39dfcea3b2927694865f45814dc1b5
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-18 00:05:18 +0300

    Updated 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,14 +18,14 @@ int main(void)
 		{
 			printf("%d", i);
 		}
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf(" FizzBuzz");
 		else if (i % 3 == 0)
 			printf(" Fizz");
 		else if (i % 5 == 0)
 			printf(" Buzz");
-		else if ((i % 3 == 0) && (i % 5 == 0))
-			printf(" FizzBuzz");
 		else
-			printf(" %d ", i);
+			printf(" %d", i);
 	}
 	putchar('\n');
 	return (0);
