72adc531ed9eea64631f7e93d520fdcb0429ff01
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-17 17:30:05 +0300

    Added 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * mul - mul function
+ *
+ * Description: the mul function
+ * puts text to stout.
+ * @a: int a
+ * @b: int b
+ * Return: void
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
