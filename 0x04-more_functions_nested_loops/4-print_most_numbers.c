518ad1952c1b37f81fc8ac00116a4b48e38b3e75
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-17 18:35:10 +0300

    Added 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * print_most_number - print_most_numbers function
+ *
+ * Description: the print_most_numbers function
+ * puts text to stout.
+ * Return: 0
+ */
+
+int print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; ++c)
+	{
+		if (c == '2' || c == '4')
+			continue;
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
+
