c1586211ce1aee5b072a42ec75d4ea411d87619b
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-17 22:10:51 +0300

    Added 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - print_diagonal function
+ *
+ * Description: the print_diagonal function
+ * puts text to stout.
+ * @n: int n
+ */
+
+void print_diagonal(int n)
+{
+	int c, i;
+
+	for (i = 0; i < n; i++)
+	{
+		for (c = 0; c < i; c++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
+
