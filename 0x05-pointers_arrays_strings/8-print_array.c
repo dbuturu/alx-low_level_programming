6c1de7f11ce1cb0f1301fdfd9079bc2aa0ecee15
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-22 01:26:37 +0300

    Added 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_array - print array
+ *
+ * @a: int *a
+ * @n: int n
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+			printf("%d\n", a[i]);
+		else
+			printf("%d, ", a[i]);
+	}
+}
+
