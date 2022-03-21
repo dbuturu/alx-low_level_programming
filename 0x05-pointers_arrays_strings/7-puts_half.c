a6925fbd482daa246621f466ae2b553d5fdf72ff
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-22 00:29:09 +0300

    Updated 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,6 +13,15 @@ void puts_half(char *str)
 	int i, n;
 
 	n = ((int)strlen(str)) / 2;
+	if (n % 2 == 1)
+	{
+		i = (n - 1) / 2;
+		i = n - i;
+	}
+	else
+	{
+		i = n / 2;
+	}
 	for (i = n; i < ((int)strlen(str)); ++i)
 		_putchar(str[i]);
 	_putchar('\n');
