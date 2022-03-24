bac5e4280dee183eca4503a8473d38287d791411
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-03-24 21:59:48 +0300

    Added 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_number - print_number.
+ *
+ * @n: The input number.
+ * Return: void.
+ */
+
+void print_number(int n)
+{
+	int m, d, i;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		m = n * -1;
+	}
+	else
+		m = n;
+
+	d = m;
+	i = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		i *= 10;
+	}
+
+	for (; i >= 1; i /= 10)
+		_putchar(((m / i) % 10) + 48);
+}
+
