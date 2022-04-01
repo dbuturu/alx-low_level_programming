f4cacb1480678d04ad378759cfd8dc47b6439490
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-04-02 00:50:55 +0300

    Added 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Adds positive numbers.
+ * @argc: Arguement count.
+ * @argv: Array of pointers to arguement strings.
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+	int i;
+	unsigned int j, sum;
+	char *x;
+
+	sum = 0;
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			x = argv[i];
+
+			for (j = 0; j < strlen(x); j++)
+			{
+				if (48 > x[j] || x[j] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			sum += atoi(x);
+		}
+
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
+
