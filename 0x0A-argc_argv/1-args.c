869d03d9cd09189357f17249ce08cf2c0bce799f
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-04-01 18:58:33 +0300

    Added 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Printsthe number of arguments passed into it.
+ * @argc: Arguement count.
+ * @argv: Array of pointers to arguement strings.
+ * Return: 0
+ **/
+
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc-1);
+	return (0);
+}
+
