06ea4cbcbaea97dcfed5da0d6c3c54698a37a902
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-04-01 20:22:19 +0300

    Updated 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,8 +10,11 @@ int main(int argc, char *argv[])
 {
 	int result;
 
-	if(argc < 2)
+	if (argc < 2)
+	{
+		printf("Error\n");
 		return (1);
+	}
 	result = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", result);
 	return (0);
