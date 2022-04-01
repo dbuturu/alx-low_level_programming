decdd182e37bd957c0ba842a8a97ac5e5f14d43d
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-04-01 20:17:08 +0300

    Added 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Multiplies two numbers.
+ * @argc: Arguement count.
+ * @argv: Array of pointers to arguement strings.
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if(argc < 2)
+		return (1);
+	result = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", result);
+	return (0);
+}
+
