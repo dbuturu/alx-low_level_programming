2f958ab3bdc0eae8dc204cdc9f06fcb284c56ed8
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-04-13 17:10:04 +0300

    Updated 3-get_op_func.c 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -5,8 +5,7 @@
  * Return: The sum of a and b.
  */
 
-int op_add(int a, int b)
-	return (a + b);
+int op_add(int a, int b) { return (a + b); }
 
 /**
  * op_sub - Returns the difference of a and b.
@@ -15,8 +14,7 @@ int op_add(int a, int b)
  * Return: The difference of a and b.
  */
 
-int op_sub(int a, int b)
-	return (a - b);
+int op_sub(int a, int b) { return (a - b); }
 
 /**
  * op_mul - Returns the product of a and b.
@@ -25,8 +23,7 @@ int op_sub(int a, int b)
  * Return: The product of a and b.
  */
 
-int op_mul(int a, int b)
-	return (a * b);
+int op_mul(int a, int b) { return (a * b); }
 
 /**
  * op_div - Returns the result of the division of a by b.
@@ -35,8 +32,7 @@ int op_mul(int a, int b)
  * Return: The division of a by b.
  */
 
-int op_div(int a, int b)
-	return (a / b);
+int op_div(int a, int b) { return (a / b); }
 
 /**
  * op_mod - Returns the remainder of the division of a by b.
@@ -45,6 +41,4 @@ int op_div(int a, int b)
  * Return: The remainder of the division of a by b.
  */
 
-int op_mod(int a, int b)
-	return (a % b);
-
+int op_mod(int a, int b) { return (a % b); }
