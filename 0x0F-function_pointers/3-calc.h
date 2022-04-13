5fb13a44b0b2ec54570ae5d56e0522da79ea660b
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-04-13 17:55:41 +0300

    Updated 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -4,13 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
-
 /**
  * struct op - Struct op
  * @op: The operator
@@ -21,6 +14,13 @@ typedef struct op
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
 
 #endif
 
