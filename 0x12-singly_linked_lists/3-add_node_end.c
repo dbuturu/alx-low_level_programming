aa9bbdccdbb8c0ee2721e339bb83f47534247f78
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-04-21 21:48:56 +0300

    Updated 3-add_node_end.c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t  *new_node_end = malloc(sizeof(list_t));
+	list_t *p = *head;
+	unsigned int j;
+	while (str[j++])
+		;
+	{
+		if (new_node_end != NULL)
+		{
+		new_node_end->str = strdup(str);
+		new_node_end->len = strlen(str);
+		new_node_end->next = NULL;
+		}
+		else
+		{
+			return (NULL);
+		}
+
+			if (*head == NULL)
+			{
+				*head = new_node_end;
+			return (*head);
+			}
+			p = *head;
+			while(p->next != NULL)
+			{
+				p = p->next;
+			}
+			p->next = new_node_end;
+
+			return (new_node_end);
+	}
+}
+
