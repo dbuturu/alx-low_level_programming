dd81818074f4083fdad99b920fa68f65a0283389
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-04-26 22:59:41 +0300

    Upated 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * free_listint_safe - Frees a listint_t list.
- * @head: The head of the list_t list.
+ * @h: The head of the list_t list.
  * Return: The size of the list that was free'd
  */
 size_t free_listint_safe(listint_t **h)
