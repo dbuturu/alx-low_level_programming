272e5b97fbc5453d6c2804958ae6a614a2f6570e
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-04-29 08:43:53 +0300

    Added files

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * putchar - putchar function
+ *
+ * Description: the main function
+ * puts text to stout.
+ * But you're totally free to use several lines to properly
+ * Return: 0
+ */
+
+int _putchar(char c)
+{
+	return (putchar(c));
+}
+
