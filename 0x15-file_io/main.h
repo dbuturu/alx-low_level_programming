df667e9e43dd712357eabf16bf1cc41e08e07013
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-05-04 05:37:07 +0300

    Updated main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,14 @@
 #ifndef MAIN
 #define MAIN
+#define _POSIX_C_SOURCE 200809L
 
+#include <elf.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char);
 ssize_t read_textfile(const char *filename, size_t letters);
@@ -12,3 +16,4 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
 #endif /* MAIN */
+
